0976cf4f5a1a0202668ec6ee238f0506e54ea35b
Bogdan Kulbida <bkulbida@gmail.com>
2016-02-05 16:10:44 -0500

    do not validate email if it is not required

--- lib/devise/models/validatable.rb
+++ lib/devise/models/validatable.rb
@@ -28,7 +28,7 @@ module Devise
         base.class_eval do
           validates_presence_of   :email, if: :email_required?
           validates_uniqueness_of :email, allow_blank: true, if: :email_changed?
-          validates_format_of     :email, with: email_regexp, allow_blank: true, if: :email_changed?
+          validates_format_of     :email, with: email_regexp, allow_blank: true, if: [:email_required?, :email_changed?]
 
           validates_presence_of     :password, if: :password_required?
           validates_confirmation_of :password, if: :password_required?
